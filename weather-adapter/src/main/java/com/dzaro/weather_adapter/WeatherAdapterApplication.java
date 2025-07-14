4e45d9127942541a049acad10a04118cc7f5065a
Jaroslaw Pawlik <jaroslaw.pawlik@pl.abb.com>
2025-07-14 11:18:14 +0200

    correct project structure

---
+++ weather-adapter/src/main/java/com/dzaro/weather_adapter/WeatherAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.dzaro.weather_adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAdapterApplication.class, args);
+	}
+
+}
---
+++ weather-adapter/src/test/java/com/dzaro/weather_adapter/WeatherAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzaro.weather_adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
